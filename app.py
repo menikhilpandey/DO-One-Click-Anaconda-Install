0fb0d5d66cfb95c763a8c3478d04a9e2a7aaaa40
Nikhil Pandey <menikhilpandey@gmail.com>
2016-11-27 05:15:17 +0530

    Added GUI for SSH Agent

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import easygui # Module to create GUI Based Application
 import dropletapi # Module to use digitalocean api to create a droplet
 import config # Module to set name, region, size of Droplet
 import auth  # Module to check for Personal Access TOKEN
-#import sshagent # Module for ssh connection with Droplet
+import sshagent # Module for ssh connection with Droplet
 
 def main():
 	
@@ -14,10 +14,20 @@ def main():
 	if confirm:
 		CONFIG = config.run()
 		dropletapi.createDroplet(TOKEN,CONFIG)
-		#sshagent.run()
+		easygui.msgbox(msg="Awesome! \
+			\nYour Ubuntu 14.04 droplet has been created.\
+			\nHOSTNAME and Password for root user have been mailed.\
+			\nCreate New Password on First login, Then click OK",
+			title="DARWIN | One-Click Ananconda2 on 14.04")
+		sshagent.run()
+		easygui.msgbox(msg="Good to go! \
+			\nDroplet with Anaconda2 on Ubuntu 14.04 Created.\
+			\nDeploy your data science projects now!",
+			title="DARWIN | One-Click Ananconda2 on 14.04")
 	else:
 		easygui.msgbox(msg="Thanks For using me!",
 						title="DARWIN | One-Click Ananconda2 on 14.04")
 
 if __name__ == '__main__':
 	main()
+	
--- sshagent.py
+++ sshagent.py
@@ -1,13 +1,32 @@
-# Module to deal with ssh connections and command execution using plink and cmd
-import os
-import easygui
-
 def run():
-	hostIP = easygui.enterbox(msg="Enter Host IP Address", 
-		title="DARWIN | One-Click Ananconda2 on 14.04"
-		)
-	os.system('plink -ssh -t root@'+hostIP)
-	#wget -O <path> <download path>
+	try:
+		from pexpect import pxssh
+		s = pxssh.pxssh()
+
+	   	hostname = easygui.enterbox(msg="Enter Host IP", 
+			title="DARWIN | One-Click Ananconda2 on 14.04")
+	   	username = easygui.enterbox(msg="Enter Username", 
+			title="DARWIN | One-Click Ananconda2 on 14.04")
+	   	password = easygui.passwordbox(msg="Enter Password",
+	    	title="DARWIN | One-Click Ananconda2 on 14.04")
+	   	
+	   	s.login(hostname, username, password)
+		
+		commands = open("commands.txt").read().split('\n')
+		commands = [command for command in commands if command!='']
+		for command in commands:
+			s.sendline(command)    # run a command
+	   		s.prompt()             # match the prompt
+	   		print(s.before)        # print everything before the prompt.
+	   	
+	   	s.logout()
+
+	except pxssh.ExceptionPxssh as e:
+		print("pxssh failed on login.")
+		print(e)
+
+	finally:
+		return None
 
 if __name__ == '__main__':
 	run()
